c6331b984d5f997957c9504cd29be6c57496f4d1
Naresh Miriyala <naresh.miriyala@redenergy.com.au>
2018-12-14 09:28:01 +1100

    Create eureka server

---
+++ src/main/groovy/au/com/redenergy/ServiceDiscovery/ServiceDiscoveryApplication.groovy
@@ -0,0 +1,14 @@
+package au.com.redenergy.ServiceDiscovery
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ServiceDiscoveryApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run ServiceDiscoveryApplication, args
+	}
+
+}
+
